e84827c4eb30e7bade1a674acc7872b2b6497572
Evelyn Lacerda <evelyndslacerda@gmail.com>
2024-12-08 04:20:37 -0300

    chore: update dependencies and adjust routes
    Updated project dependencies by adding and removing specific packages to
    align with current requirements. Adjusted routes to ensure compatibility
    with the updated setup and maintain consistent functionality across the
    application.

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,19 @@
-import Home from "@/pages/home/page"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Structor from "@/layouts/Structor";
+import Home from "@/pages/home/page";
+import Order from "@/pages/order/page";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={ <Home/> } />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route element={<Structor />}>
+					<Route path="/pedido" element={<Order />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
-export default Router
+export default Router;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+	darkMode: ["class"],
+	content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
 	theme: {
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		colors: {}
-    	}
-    },
+		extend: {
+			fontFamily: {
+				figtree: ["Figtree", "sans-serif"],
+				rubik: ["Rubik", "sans-serif"],
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			colors: {
+				gray: {
+					"light": "#D2D6D8",
+					"medium": "#565D62",
+					"dark": "#44494D",
+					"50opacity": "rgba(68,73,77,0.50)",
+					"25opacity": "rgba(68,73,77,0.25)",
+					"10opacity": "rgba(68,73,77,0.1)",
+				},
+				pink: {
+					"light": "#FEE8D0",
+					"dark": "#D79489",
+					"25opacity": "rgba(215,148,137,0.25)",
+				},
+				green: {
+					"light": "#9ec9ab",
+					"dark": "#4fb567",
+				},
+			},
+		},
+	},
 	plugins: [require("tailwindcss-animate")],
 };
 
