3b2682436568b218feda916df0d8937bd3561eea
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2024-12-22 19:51:23 -0300

    feat: Calcula a quantidade de salgados a partir da quantidade selecionada e refatoracoes no código

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Router from './routes/Router'
 import '@/assets/css/global.css'
 import '@/assets/css/style.css'
+import { SelectedItemsProvider } from './context/selectedItemsProvider'
 
 function App() {
 
   return (
-    <>
+    <SelectedItemsProvider>
       <Router />
-    </>
+    </SelectedItemsProvider>
   )
 }
 
